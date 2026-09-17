62112282dacb4bbfd4a8ceba4f279908303c56ab
nhmalang <zisnhmalang2@gmail.com>
2026-09-17 10:59:13 +0700

    ganti code import React, { useState, useEffect } from 'react';
    sebelumnya import { useState, useEffect } from 'react';

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
